2c0a02f63aded25fec320ce9f795563fdb8ff882
choijiwon <gwonwon1201@gmail.com>
2023-01-17 14:15:25 +0900

    [settings] 스프링 프로젝트 생성및 환경설정 S08P11D211-16

---
+++ src/main/java/com/ssafy/metassafy/MetassafyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.metassafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetassafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetassafyApplication.class, args);
+	}
+
+}
