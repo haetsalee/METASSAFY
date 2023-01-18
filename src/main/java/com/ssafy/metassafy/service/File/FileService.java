2502b84626d35456f639e7780e60e9ce1465d9e2
이석원 <tjrdnjs67@naver.com>
2023-01-18 16:38:32 +0900

    [fix] 환경 설정 fix

---
+++ src/main/java/com/ssafy/metassafy/service/File/FileService.java
@@ -0,0 +1,4 @@
+package com.ssafy.metassafy.service.File;
+
+public interface FileService {
+}
---
+++ src/main/java/com/ssafy/metassafy/service/File/FileServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ssafy.metassafy.service.File;
+
+public class FileServiceImpl implements FileService{
+}
