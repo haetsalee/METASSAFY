5c31e2490263a92fcb4c4bd7eccb810ef52d792e
jiwon <jiwonchoi99@naver.com>
2023-01-19 22:56:57 +0900

    [feat] 친구 틀 잡는 중

---
+++ src/main/java/com/ssafy/metassafy/controller/friend/friendController.java
@@ -0,0 +1,18 @@
+package com.ssafy.metassafy.controller.friend;
+
+
+public class friendController {
+    //user_id의 친구 목록 조회
+    //사용자 검색
+
+    //친구 삭제
+    //친구 수 조회
+
+    //친구 신청 보내기
+    //친구 신청 온거 알림 조회
+    //신청 수락(둘을 친구로 만드는 로직 추가하기)
+
+    //신청 거절
+
+
+}
---
+++ src/main/java/com/ssafy/metassafy/dto/friend/friendDto.java
@@ -0,0 +1,4 @@
+package com.ssafy.metassafy.dto.friend;
+
+public class friendDto {
+}
