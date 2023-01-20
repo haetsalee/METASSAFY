aed2c5c09a68de2f7dbf3d2451c597c9e87c4dcd
Lee Sijun <season2lee@gmail.com>
2023-01-20 10:30:01 +0900

    [test] socket.io 연결 포트 변경

--- src/three/Canvas.js
+++ src/three/Canvas.js
@@ -11,7 +11,7 @@ import { AnimationClip, AnimationMixer } from "three";
 
 import Stats from "three/examples/jsm/libs/stats.module";
 
-const socket = io.connect("http://localhost:8080", {
+const socket = io.connect("http://localhost:8090", {
   path: "/socket.io",
   transports: ["websocket"],
 });
