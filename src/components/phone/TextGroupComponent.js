2239410a66a1351e52ed32562cb24cc0a328841e
werr900 <werr900@naver.com>
2023-01-26 06:32:34 +0900

    🍀[FE: style] phone 디자인 수정

--- src/components/phone/TextGroupComponent.js
+++ src/components/phone/TextGroupComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 function TextGroupComponent(props) {
     return <div style={{textAlign: 'center', margin: 20}}>
         <b style={{margin: 0, padding: 0, fontSize: '1.5rem'}}>{props.name}</b> <br/>
-        <b>{props.class}</b>
+        <b style={{margin: 0, padding: 0, fontSize: '0.8rem'}}>{props.class}</b>
     </div>
 }
 
--- src/components/ui/Phone.js
+++ src/components/ui/Phone.js
@@ -1,3 +1,4 @@
+import BackgroundBox from '../phone/BackgroundBox';
 import MajorPositionClass from '../phone/MajorPositionClass';
 import TextGroupComponent from '../phone/TextGroupComponent';
 import classes from './Phone.module.css'
@@ -5,15 +6,10 @@ import classes from './Phone.module.css'
 function Phone(props) {
   return <div className={classes.phone}>
     {props.children}
-    프로필 사진
-      활동중 뱃지
-      이름
-      반
+      <BackgroundBox />
       <TextGroupComponent name='김싸피' class='구미 2반'/>
+      {/* 전공, 포지션, 공통 */}
       <MajorPositionClass />
-      전공
-      포지션
-      공통
       기술스택
       자기소개
       생일
