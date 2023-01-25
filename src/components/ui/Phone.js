3ade3b2484c862de824741c4227642ab12737123
werr900 <werr900@naver.com>
2023-01-26 05:20:14 +0900

    🍀[FE: style] Phone component 기초 세팅

---
+++ src/components/ui/Phone.js
@@ -0,0 +1,21 @@
+import TextGroupComponent from '../phone/TextGroupComponent';
+import classes from './Phone.module.css'
+
+function Phone(props) {
+  return <div className={classes.phone}>
+    {props.children}
+    프로필 사진
+      활동중 뱃지
+      이름
+      반
+      <TextGroupComponent name='김싸피' class='구미 2반'/>
+      전공
+      포지션
+      공통
+      기술스택
+      자기소개
+      생일
+  </div>
+}
+
+export default Phone;
--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,12 +1,26 @@
 import { useHistory } from "react-router-dom";
+import TextGroupComponent from "../components/phone/TextGroupComponent";
+import Phone from "../components/ui/Phone";
 
 
 function Page1() {
 
 
-  return <section>
+  return <section style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
     <h1>Page1</h1>
-    
+    <Phone>
+      {/* 프로필 사진
+      활동중 뱃지
+      이름
+      반
+      <TextGroupComponent name='김싸피' class='구미 2반'/>
+      전공
+      포지션
+      공통
+      기술스택
+      자기소개
+      생일 */}
+    </Phone>
   </section>;
 }
 
