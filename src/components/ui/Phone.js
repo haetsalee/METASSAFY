4364ce746812e83e977a30b387ae104417970aac
werr900 <werr900@naver.com>
2023-01-26 05:48:44 +0900

    🍀[FE: fix] phone 구성 수정

--- src/components/ui/Phone.js
+++ src/components/ui/Phone.js
@@ -1,3 +1,4 @@
+import MajorPositionClass from '../phone/MajorPositionClass';
 import TextGroupComponent from '../phone/TextGroupComponent';
 import classes from './Phone.module.css'
 
@@ -9,6 +10,7 @@ function Phone(props) {
       이름
       반
       <TextGroupComponent name='김싸피' class='구미 2반'/>
+      <MajorPositionClass />
       전공
       포지션
       공통
