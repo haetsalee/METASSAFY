78a92b4cdc8e6faa525a0a19d41420cf50a7c33c
이석원 <tjrdnjs67@naver.com>
2023-01-25 09:46:47 +0900

    🌚[fix] UnsatisfiedDependencyException 에러 수정

--- src/main/java/com/ssafy/metassafy/mapper/memo/MemoMapper.java
+++ src/main/java/com/ssafy/metassafy/mapper/memo/MemoMapper.java
@@ -2,10 +2,12 @@ package com.ssafy.metassafy.mapper.memo;
 
 import com.ssafy.metassafy.dto.memo.MemoDto;
 import com.ssafy.metassafy.dto.memo.MemoParameterDto;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@Mapper
 public interface MemoMapper {
     public List<MemoDto> listMemo(MemoParameterDto memoParameterDto) throws SQLException;
     public int writeMemo(MemoDto memoDto) throws SQLException;
