c0bf6d30e5d54df111d821bfadc28684e1b7edd7
werr900 <werr900@naver.com>
2023-01-30 13:29:59 +0900

    🍀[FE: refactor] MajorPositionClass refactor

--- src/components/phone/MajorPositionClass.jsx
+++ src/components/phone/MajorPositionClass.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import TextGroup from './TextGroup';
-import classes from './MajorPositionClass.module.css';
+import styled from 'styled-components';
 
 function MajorPositionClass() {
-  // return <div style={{display: 'flex', justifyContent: 'space-around' }}>
   return (
-    <div className={classes.box}>
+    <MajorPositionClassStyle>
       <TextGroup name="Java" class="전공" />
       <TextGroup name="FE" class="포지션" />
       <TextGroup name="D211ER" class="공통" />
-    </div>
+    </MajorPositionClassStyle>
   );
 }
 
 export default MajorPositionClass;
+
+const MajorPositionClassStyle = styled.div`
+  background-color: #e0f4ff;
+  border-radius: 6rem 0 0 6rem;
+  box-shadow: inset 0 0.4rem 0.4rem rgba(0, 0, 0, 0.0500000007);
+  box-sizing: border-box;
+  flex-shrink: 0;
+  display: flex;
+  align-content: center;
+  justify-content: space-around;
+  width: 90%;
+  margin: 0rem 0rem 5rem 0rem;
+  padding: 0.5rem;
+`;
