4f5f03231901303780dd45c7cc18cf2f2d77465b
werr900 <werr900@naver.com>
2023-01-30 09:35:57 +0900

    🍀[FE: refactor] Card.jsx refactoring
    JIRA Code : S08P12D211-253

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,9 +1,15 @@
-import classes from './Card.module.css'
+import styled from 'styled-components';
 
 function Card(props) {
-  return <div className={classes.card}>
+  return <CardStyle>
     {props.children}
-  </div>
+  </CardStyle>
 }
 
 export default Card;
+
+const CardStyle = styled.div`
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+`
