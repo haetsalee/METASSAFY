25600cf98f7f471f50de25ada63a8e3e423ecf07
werr900 <werr900@naver.com>
2023-01-30 09:25:32 +0900

    🍀[FE: refactor] PhoneUserProfile refactoring
    JIRA code: S08P12D211-253

--- src/pages/phone_pages/PhoneUserProfile.jsx
+++ src/pages/phone_pages/PhoneUserProfile.jsx
@@ -1,11 +1,12 @@
 import TextGroupComponent from '../../components/phone/TextGroupComponent';
-import classes from './PhoneUserProfile.module.css'
 
 import MajorPositionClass from '../../components/phone/MajorPositionClass';
 import BackgroundBox from '../../components/phone/BackgroundBox';
 
+import styled from 'styled-components';
+
 function PhoneUserProfile(props) {
-  return <div className={classes.phone_user_profile}>
+  return <PhoneUserProfileStyle>
       <BackgroundBox />
       <TextGroupComponent name='??' class='???'/>
       {/* 전공, 포지션, 공통 */}
@@ -13,7 +14,21 @@ function PhoneUserProfile(props) {
       기술스택
       자기소개
       생일
-  </div>
+  </PhoneUserProfileStyle>
 }
 
 export default PhoneUserProfile;
+
+const PhoneUserProfileStyle = styled.div`
+background-color: rgb(247, 254, 255);
+border-radius: 1rem;
+border: 10px rgb(121, 190, 232) solid;
+width: 20rem;
+height: 40rem;
+box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+position: relative;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
