859a820a78297a574f0c47a9549cb8cdaf7f1724
werr900 <werr900@naver.com>
2023-01-30 15:30:08 +0900

    🍀[FE: fix] port localhost to our site

--- src/Socket.js
+++ src/Socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-export const socket = io.connect('http://localhost:8090', {
+// export const socket = io.connect('http://localhost:8090', {
+export const socket = io.connect('http://i8d211.p.ssafy.io:8089/', {
   path: '/socket.io',
   transports: ['websocket'],
 });
