30d9831f61cb063baf9bdd40476463c9ccb913f6
werr900 <werr900@naver.com>
2023-01-30 15:09:13 +0900

    🍀[FE: fix] fix port 5000 to 8090

--- src/Socket.js
+++ src/Socket.js
@@ -1,6 +1,6 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 
-export const socket = io.connect("http://localhost:5000", {
-  path: "/socket.io",
-  transports: ["websocket"],
+export const socket = io.connect('http://localhost:8090', {
+  path: '/socket.io',
+  transports: ['websocket'],
 });
