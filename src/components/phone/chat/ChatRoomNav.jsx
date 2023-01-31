2ef72164685b3e6edeb25966d661b280fd1c862a
Lee Sijun <season2lee@gmail.com>
2023-01-31 16:06:32 +0900

    💎[FE: feat, setting] 채팅방 외관 진행중, icon 추가

--- src/components/phone/chat/ChatRoomNav.jsx
+++ src/components/phone/chat/ChatRoomNav.jsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+import ChatRoomNavStatus from './ChatRoomNavStatus';
+
 function ChatRoomNav() {
-  return <div>채팅방 이름과 사람 목록</div>;
+  return (
+    <ChatRoomNavStyle>
+      {/* <ChatRoomNavImgStyle
+        src="https://images.ctfassets.net/hrltx12pl8hq/7JnR6tVVwDyUM8Cbci3GtJ/bf74366cff2ba271471725d0b0ef418c/shutterstock_376532611-og.jpg"
+        alt=""
+      /> */}
+      <ChatRoomNavStatus />
+    </ChatRoomNavStyle>
+  );
 }
 
 export default ChatRoomNav;
+
+const ChatRoomNavStyle = styled.div`
+  padding: 0.5rem 2rem 0.5rem 2rem;
+  display: flex;
+  align-self: start;
+`;
+
+// const ChatRoomNavImgStyle = styled.img`
+//   width: 2rem;
+//   height: 2rem;
+//   border-radius: 50%;
+//   margin-right: 0.5rem;
+// `;
--- src/components/phone/chat/ChatRoomNavStatus.jsx
+++ src/components/phone/chat/ChatRoomNavStatus.jsx
@@ -1,5 +1,24 @@
+import styled from 'styled-components';
+
 function ChatRoomNavStatus() {
-  return <div></div>;
+  return (
+    <ChatRoomNavStatusStyle>
+      <ChatRoomNameStyle>김싸피와 채팅채팅</ChatRoomNameStyle>
+      <ChatRoomMemberStyle>ㅇㅅㅇ...</ChatRoomMemberStyle>
+    </ChatRoomNavStatusStyle>
+  );
 }
 
 export default ChatRoomNavStatus;
+
+const ChatRoomNavStatusStyle = styled.div`
+  padding: 0.3rem 0.3rem 0.3rem 0.3rem;
+`;
+
+const ChatRoomNameStyle = styled.p`
+  font-size: 1.1rem;
+`;
+
+const ChatRoomMemberStyle = styled.p`
+  font-size: 0.3rem;
+`;
