d5622b393d11a2e7dae22fc8e936197ad10aa087
werr900 <werr900@naver.com>
2023-02-01 15:13:52 +0900

    🍀[FE: feat] for localhost

--- src/Socket.js
+++ src/Socket.js
@@ -1,7 +1,7 @@
 // import { io } from 'socket.io-client';
 
-// // export const socket = io.connect('http://localhost:8090', {
-// export const socket = io.connect('http://i8d211.p.ssafy.io:8090/', {
+// export const socket = io.connect('http://localhost:8090', {
+//   // export const socket = io.connect('http://i8d211.p.ssafy.io:8090/', {
 //   path: '/socket.io',
 //   transports: ['websocket'],
 // });
@@ -11,8 +11,10 @@ import { io } from 'socket.io-client';
 export let socket = null;
 
 export function connectSocket() {
-  socket = io.connect('http://i8d211.p.ssafy.io:8090/', {
-    // socket = io.connect('http://localhost:8090/', {
+  // 빌드용
+  // socket = io.connect('http://i8d211.p.ssafy.io:8090/', {
+  // 로컬 테스트용 -
+  socket = io.connect('http://localhost:8090/', {
     path: '/socket.io',
     transports: ['websocket'],
   });
