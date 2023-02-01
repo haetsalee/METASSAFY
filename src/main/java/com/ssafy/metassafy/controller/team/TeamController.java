e7c336b0acca6ba26ab156f3404c846c3ff417fb
choijiwon <gwonwon1201@gmail.com>
2023-02-01 13:15:58 +0900

    🪑[BE:feat] 토큰 만료시간 24시간으로 S08P12D211-262

--- src/main/java/com/ssafy/metassafy/controller/team/TeamController.java
+++ src/main/java/com/ssafy/metassafy/controller/team/TeamController.java
@@ -1,15 +1,16 @@
 package com.ssafy.metassafy.controller.team;
 
 import com.ssafy.metassafy.controller.friend.FriendController;
+import com.ssafy.metassafy.dto.team.Team;
+import com.ssafy.metassafy.service.friend.FriendService;
+import com.ssafy.metassafy.service.team.TeamService;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/team")
@@ -19,9 +20,12 @@ public class TeamController {
     private static final Logger logger = LoggerFactory.getLogger(FriendController.class);
     private static final String SUCCESS = "Success";
     private static final String FAIL = "Fail";
+    @Autowired
+    private TeamService service;
 
     @PostMapping("/makeTeam")
-    public ResponseEntity<String> makeTeam(){
+    public ResponseEntity<String> makeTeam(@RequestBody Team team){
+        service.makeTeam(team);
         return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
     }
 
