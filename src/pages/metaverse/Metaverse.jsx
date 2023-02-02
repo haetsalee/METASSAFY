b083fccee0331167a5a3ef6a790ff218c53e2bc7
werr900 <werr900@naver.com>
2023-02-02 09:34:52 +0900

    🍀[FE: feat] Metaverse 구축 - canvas 생성

---
+++ src/pages/metaverse/Metaverse.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// gltfLoader사용
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Player } from './Player';
+import { House } from './House';
+// import { Map } from './Map';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import gsap from 'gsap';
+
+function Metaverse() {
+  const canvasRef = useRef(null); // useRef사용
+  const [canvasTag, setCanvasTag] = useState([]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 1000;
+
+    setCanvasTag(canvas);
+  }, []);
+
+  return (
+    <div
+      className="canvas_Wrap"
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      {/* <canvas id="myThreeJsCanvas"></canvas>;    */}
+      {/* <Card> */}
+      <canvas className="meta-ssafy2" ref={canvasRef}></canvas>
+      {/* </Card> */}
+    </div>
+  );
+}
+
+export default Metaverse;
