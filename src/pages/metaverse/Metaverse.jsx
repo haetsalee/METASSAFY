3eab2425d06d86878afd0d0646236c0d233e79a0
werr900 <werr900@naver.com>
2023-02-02 09:37:27 +0900

    🍀[FE: feat] Metaverse구축 - renderer

--- src/pages/metaverse/Metaverse.jsx
+++ src/pages/metaverse/Metaverse.jsx
@@ -27,6 +27,17 @@ function Metaverse() {
     floorTexture.wrapT = THREE.RepeatWrapping;
     floorTexture.repeat.x = 10;
     floorTexture.repeat.y = 10;
+
+    // Renderer
+    // const canvas = document.querySelector('#three-canvas');
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   }, []);
 
   return (
