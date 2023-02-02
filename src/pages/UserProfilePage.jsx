4e79fb827ee177d8ae9541e5679671e34f05d5d3
werr900 <werr900@naver.com>
2023-02-02 13:00:00 +0900

    🍀[FE: feat] UserProfilePage 컴포넌트

---
+++ src/pages/UserProfilePage.jsx
@@ -0,0 +1,22 @@
+import { useHistory } from 'react-router-dom';
+import TextGroupComponent from '../components/phone/TextGroupComponent';
+import Phone from '../components/UI/Phone';
+import PhoneUserProfileById from './phone_pages/PhoneUserProfileById';
+import GetUserStack from '../components/phone/GetUserStack';
+import { useEffect } from 'react';
+
+function UserProfilePage() {
+  return (
+    <section
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <h1>UserProfilePage</h1>
+      <Phone>
+        <PhoneUserProfileById name="admin"></PhoneUserProfileById>
+      </Phone>
+      <GetUserStack></GetUserStack>
+    </section>
+  );
+}
+
+export default UserProfilePage;
