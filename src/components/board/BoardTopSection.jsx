5bcde77bf8349f020db863aaae33594a540f825f
윤소현 <wja06018@naver.com>
2023-02-08 01:16:06 +0900

    🎸[FE: feat] 게시판 상단바

--- src/components/board/BoardTopSection.jsx
+++ src/components/board/BoardTopSection.jsx
@@ -1,12 +1,31 @@
 import styled from 'styled-components';
 
 const BoardTopSection = () => {
-  return <SectionStyle>hi</SectionStyle>;
+  return (
+    <SectionStyle>
+      <h1>커뮤니티</h1>
+      <p>메타싸피인들의 소통 공간</p>
+    </SectionStyle>
+  );
 };
 
 export default BoardTopSection;
 
 const SectionStyle = styled.section`
-  background-color: pink;
+  background-color: #6e7f88;
   height: 20rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+
+  & h1 {
+    font-size: 2rem;
+    margin-top: 3rem;
+    margin-bottom: 0.4rem;
+  }
+  & p {
+    opacity: 0.6;
+  }
 `;
