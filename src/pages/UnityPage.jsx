c2024c6a28ca5f632cebc9d3da327b874f22605a
choijiwon <gwonwon1201@gmail.com>
2023-02-07 16:27:21 +0900

    🪑[FE:feat] 캐릭터 선택 추가
    Former-commit-id: 6e285952d7f9c7adb6b15b6a6f9edbb41b373e2e

--- src/pages/UnityPage.jsx
+++ src/pages/UnityPage.jsx
@@ -29,7 +29,7 @@ function UnityPage() {
   useEffect(() => {
     if (isLoaded) {
       console.log(loginUser.user_id + ' 가 메타싸피에 접속');
-      sendMessage('ReactManager', 'getUserId', loginUser.user_id);
+      sendMessage('ValueManager', 'getUserId', loginUser.user_id);
     }
   }, [isLoaded]);
 
