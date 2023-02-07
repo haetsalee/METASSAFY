02495f7f26acb418a5b8e3edcda32a2f53a6cad2
werr900 <werr900@naver.com>
2023-02-07 11:33:56 +0900

    🍀[FE: feat] PrivateRoute 로그인 안한경우 메세지 설정
    Former-commit-id: bd126b324db4be2395ebc9abca0d587be563960f

--- src/libs/PrivateRoute.js
+++ src/libs/PrivateRoute.js
@@ -4,9 +4,9 @@ import isLogin from './isLogin';
 
 // 로그인한 유저에게만 라우팅
 // 로그인하지 않았으면 (로그인 모달이 있는)메인페이지로 redirect
-const PrivateRoute = ({ redirectPath = '/' }) => {
+const PrivateRoute = ({ redirectPath = '/login' }) => {
   if (!isLogin()) {
-    window.alert('로그인하슈!');
+    window.alert('로그인이 필요합니다.');
     return <Navigate to={redirectPath} />;
   }
 
