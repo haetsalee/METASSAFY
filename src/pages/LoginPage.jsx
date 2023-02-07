9ee8c8187c30ff46ef17767519b7f040af262a71
werr900 <werr900@naver.com>
2023-02-07 11:35:12 +0900

    🍀[FE: feat] 컴퓨터용 LoginPage 생성
    Former-commit-id: b3583c20a1e2ee58c7cd3ce89832135e1398fc4a

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,24 @@
+import AuthTitle from '../components/auth/AuthTitle';
+import AuthClose from '../components/auth/login/AuthClose';
+import LoginBottom from '../components/auth/login/LoginBottom';
+import LoginPageForm from '../components/auth/login/LoginPageForm';
+import styled from 'styled-components';
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <LoginPageStyle>
+      <div>
+        {/* <AuthClose onClose={props.onClose} /> */}
+        <AuthTitle title="METASSAFY!" subTitle="DIVE TO" />
+        <LoginPageForm />
+        <LoginBottom />
+      </div>
+    </LoginPageStyle>
   );
 }
+
+const LoginPageStyle = styled.div`
+  /* background-color: white; */
+  display: flex;
+  justify-content: center;
+`;
