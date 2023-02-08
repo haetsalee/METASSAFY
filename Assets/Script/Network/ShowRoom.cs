70eae37798ebb0dbc6282679d05d959595f17967
choijiwon <gwonwon1201@gmail.com>
2023-02-08 16:15:41 +0900

    🪑[Unity:feat] 참여자 목록 추가
    Former-commit-id: 53be46ffd2f16ef8f3980bcc0666de01877e2715
    Former-commit-id: 740b9b3ca505744fa20ac480bed5533637bef49a

--- Assets/Script/Network/ShowRoom.cs
+++ Assets/Script/Network/ShowRoom.cs
@@ -4,35 +4,54 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowRoom : MonoBehaviourPunCallbacks
 {
     public PhotonView PV;
-    Boolean isInit = false;
+    Boolean isOpen = false;
+    public GameObject infoArea;
     void Start()
     {
-         
+        
     }
+    public override void OnJoinedRoom()
+    {
+        infoArea.GetComponent<Transform>().Find("RoomName").GetComponent<Text>().text = PhotonNetwork.CurrentRoom.Name+"의 참여자들";
+        PV.RPC("addPlayer", RpcTarget.AllBuffered, null);
+        //initRoomInfo();
 
+    }
     // Update is called once per frame
     void Update()
     {
-        if (!isInit&&PhotonNetwork.InRoom)
-        {
-            showRoomInfo();
-           // PV.RPC("addPlayer", RpcTarget.AllBuffered, PV.ViewID);
-        }
+         
     }
-    void showRoomInfo()
+    void initRoomInfo()
     {
         Debug.Log(PhotonNetwork.CurrentRoom.MaxPlayers);
         Debug.Log(PhotonNetwork.PlayerList.Length);
-        isInit= true;
+       
     }
     [PunRPC]
     private void addPlayer()
     {
         Debug.Log("플레이어 입장");
-
+        string playerList=infoArea.GetComponent<Transform>().Find("playerList").GetComponent<Text>().text;
+        playerList += PhotonNetwork.LocalPlayer.NickName + "\n";
+        infoArea.GetComponent<Transform>().Find("playerList").GetComponent<Text>().text = playerList;
+       
+    }
+    public void onClickOpen()
+    {
+        isOpen = !isOpen;
+        if (isOpen)
+        {
+            infoArea.SetActive(true);
+        }
+        else
+        {
+            infoArea.SetActive(false);
+        }
     }
 }
