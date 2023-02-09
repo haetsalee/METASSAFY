e6655708602486a9571743dcca9b37962d15d955
choijiwon <gwonwon1201@gmail.com>
2023-02-09 17:36:51 +0900

    🪑[untiy:feat] 데드존 추가
    Former-commit-id: 7c3b33aaa3a70ae6264802aae3945ed9c048a614
    Former-commit-id: 522428ee45ff430f16a99ab2cebe9951aafd08bc

--- Assets/Script/Gumi/respawn.cs
+++ Assets/Script/Gumi/respawn.cs
@@ -18,50 +18,17 @@ public class respawn : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter(Collision collision)
-
-    {
-
-        // 이 컴포넌트가 부착된 게임 오브젝트의 콜라이더와 충돌한 게임 오브젝트 가져오기
-
-        var obj = collision.gameObject;
-
-        // 특정 컴포넌트 가져오기
-
-      
-
-        Debug.Log("충돌!"+obj.name);
-
-    }
+   
     private void OnTriggerEnter(Collider other)
 
     {
 
-        Debug.Log(other.name + "감지 시작!");
-        var obj = other.gameObject;
-
-        // 특정 컴포넌트 가져오기
-
+        Debug.Log(other.name + "데드존에 닿았음!");
+       
         
-        reset();
     }
 
-    public void reset()
-    {
-        //모든 플레이어 중에서
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        for (int i = 0; i < players.Length; i++)
-        {    //이동한 애(=자기 자신)을 리액트에서 보내준 이름으로 설정
-            if (players[i].GetComponent<PhotonView>().IsMine)
-            {
-                Debug.Log(players[i].transform.position + "에서 ");
-                players[i].transform.position = Vector3.zero;
-                
-                Debug.Log(players[i].transform.position+ "이동!");
-               
-            }
-        }
-    }
+   
 
 
 
