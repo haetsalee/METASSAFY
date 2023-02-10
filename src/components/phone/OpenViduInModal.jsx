bc955b7277f644c926c71ff2ecc6d95e3af1e971
werr900 <werr900@naver.com>
2023-02-10 21:57:27 +0900

    🍀[FE: feat] OpenViduInModal 생성
    Former-commit-id: 961b9b20c3de7693e5940c99c7faa110d43ef6c6
    Former-commit-id: 839d78bbb82b42f40d03deb668cd20c88b78a28e

---
+++ src/components/phone/OpenViduInModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PhoneModal from '../UI/modal/PhoneModal';
+import PhoneClose from './PhoneClose';
+import PhoneFriendPage from '../../pages/phone_pages/PhoneFriendPage';
+import OpenViduPage from '../../pages/OpenViduPage';
+
+const OpenViduInModal = (props) => {
+  return (
+    <PhoneModal>
+      <PhoneClose onClose={props.onClose} />
+      <OpenViduPage></OpenViduPage>
+    </PhoneModal>
+  );
+};
+
+export default OpenViduInModal;
