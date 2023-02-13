8dd1cab4e9f18524b10e5e84d9d5058786e9fcda
choijiwon <gwonwon1201@gmail.com>
2023-02-13 22:04:18 +0900

    🪑[Unity:feat] 음악 선택 추가
    Former-commit-id: 9cb48a221d029e0457cc39b86f60a92f319f85e4
    Former-commit-id: d8c8602f1825b8347e3a6904500a901bb498c6e8

---
+++ Assets/Script/Lobby/VideoManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "ssafy_video.webm");
+    }
+}
