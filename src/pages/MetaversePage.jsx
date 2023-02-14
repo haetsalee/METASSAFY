263589e820f2a198724668f1a53ed3de353ea6bf
werr900 <werr900@naver.com>
2023-02-14 17:53:48 +0900

    🍀[FE: fix] MetaversePage name 변경
    Former-commit-id: eb9748a6b45c2d022c6b4ad556822f55937e8ac9
    Former-commit-id: 540829c2ddc15f8c47ff0e2ced5c61756a917856

---
+++ src/pages/MetaversePage.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router';
+import styled from 'styled-components';
+import MetaverseThree from './metaverse/MetaverseThree';
+
+const MetaversePage = () => {
+  return (
+    <FlexDiv>
+      {/* <h1>meta</h1> */}
+      <Outlet />
+      <MetaverseThree />
+    </FlexDiv>
+  );
+};
+
+export default MetaversePage;
+
+const FlexDiv = styled.div`
+  position: relative;
+`;
