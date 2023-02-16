6ea478133294ce19590c638da5a669ecddf9b776
sohyeonyun <wja06018@naver.com>
2023-02-16 15:48:51 +0900

    🎸[FE: feat] 리덕스 로그 삭제
    Former-commit-id: 355776ccb86dbddda012b25a4d97322f3723a0f0
    Former-commit-id: 4c39563b541b6a7768ebaf07d80341026e18c114

--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,7 @@ const rootReducer = combineReducers({
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
