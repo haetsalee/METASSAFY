14fe56d8321ecce12e6fb24fca54788ba231ce84
Lee Sijun <season2lee@gmail.com>
2023-02-20 15:59:24 +0900

    💎[FE: fix] board list date getMonth + 1
    Former-commit-id: f42131d8684a6689ffe8ffd06895c24daac9d62e
    Former-commit-id: 0183d59104a30b9046ab09b3645bb6a7fd953848

--- src/components/board/list/BoardCard.jsx
+++ src/components/board/list/BoardCard.jsx
@@ -7,7 +7,7 @@ const BoardCard = ({ card, setBoardList }) => {
   const strDate =
     String(date.getFullYear()).slice(2, 4) +
     '.' +
-    String(date.getMonth()).padStart(2, '0') +
+    String(date.getMonth() + 1).padStart(2, '0') +
     '.' +
     String(date.getDate()).padStart(2, '0');
 
